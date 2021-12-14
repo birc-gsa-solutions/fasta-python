cdd329d24ee0139b5da6b379c4937db584d95034
Thomas Mailund <mailund@birc.au.dk>
2021-12-14 08:00:03 +0100

    better testing script

--- test-get-subseqs.sh
+++ test-get-subseqs.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+build_cmd() {
+    newstr=()
+    for cnt in $* ;do
+        [ "${cnt:0:1}" == '$' ] && cnt=${cnt:1} && cnt=${!cnt}
+        newstr+=($cnt)
+    done
+    echo "${newstr[*]}"
+}
+
 success=1
 test_expected() {
     for f in test-data/*.fa; do
         coords=$f-coordinates
         expected=$f-get-subseqs-expected
-        args=`eval echo $1`
-        if ! cmp -s <(./get-subseqs $args) $expected; then
-            echo "./get-subseqs $args did not produce the expected output"
-            diff <(./get-subseqs $args) $expected
+        local cmd=$(build_cmd $1)
+        if ! cmp -s <(eval $cmd) $expected; then
+            echo "$cmd did not produce the expected output"
+            diff <(eval $cmd) $expected
             echo
             success=0
         fi
@@ -16,13 +25,13 @@ test_expected() {
 }
 
 echo "Providing coordinates in a file"
-test_expected "\$f \$coords"
+test_expected './get-subseqs $f $coords'
 
 echo "Providing coordinates on stdin"
-test_expected "\$f < \$coords"
+test_expected 'cat $coords | ./get-subseqs $f'
 
-echo "Providing coordinates as `-` (stdin)"
-test_expected "\$f - < \$coords"
+echo "Providing coordinates as '-' (stdin)"
+test_expected 'cat $coords | ./get-subseqs $f -'
 
 if (( success == 1 )); then
     echo "Success."
