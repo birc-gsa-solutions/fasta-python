b2e6db9bdc08cf47c1bb382c0b10ff0f97378625
Thomas Mailund <mailund@birc.au.dk>
2022-10-04 10:28:13 +0200

    new test framework

--- .test/test-fasta-recs.sh
+++ .test/test-fasta-recs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 success=1
-for f in test-data/*.fa; do
+for f in .test/test-data/*.fa; do
     if ! cmp -s <(./fasta-recs $f) $f-fasta-recs-expected; then
         echo "./fasta-recs $f did not produce the expected output"
         diff <(./fasta-recs $f) $f-fasta-recs-expected
